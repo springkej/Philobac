619a10fe1245f5aa7ce6c5b46d4555d65cb942de
springkej <springkej4@gmail.com>
2026-05-24 14:21:52 +0100

    Github changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/philobac/', 
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
